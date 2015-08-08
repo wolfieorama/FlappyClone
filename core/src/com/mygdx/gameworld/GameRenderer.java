3b1eeb0219417f0e5f17cff04d5dae0e714a0f82
Sylvia <sylviakieha@gmail.com>
2015-08-09 01:21:00 +0300

    added pipes to game

--- core/src/com/mygdx/gameworld/GameRenderer.java
+++ core/src/com/mygdx/gameworld/GameRenderer.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.gameobjects.Bird;
+import com.mygdx.gameobjects.Grass;
+import com.mygdx.gameobjects.Pipe;
+import com.mygdx.gameobjects.ScrollHandler;
 import com.mygdx.zbHelpers.AssetLoader;
 
 
@@ -16,6 +19,9 @@ import com.mygdx.zbHelpers.AssetLoader;
  */
 public class GameRenderer {
     private Bird bird;
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
 
     private TextureRegion bg, grass;
     private Animation birdAnimation;
@@ -105,6 +111,12 @@ public class GameRenderer {
 
     private void initGameObjects() {
         bird = myWorld.getBird();
+        scroller = myWorld.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
     }
 
     private void initAssets() {
@@ -117,5 +129,52 @@ public class GameRenderer {
         skullUp = AssetLoader.skullUp;
         skullDown = AssetLoader.skullDown;
         bar = AssetLoader.bar;
+    }
+
+    private void drawGrass() {
+        // Draw the grass
+        batcher.draw(grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+
+        batcher.draw(skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+        batcher.draw(bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
     }
 }
--- core/src/com/mygdx/gameworld/GameWorld.java
+++ core/src/com/mygdx/gameworld/GameWorld.java
@@ -1,22 +1,35 @@
 package com.mygdx.gameworld;
 
 import com.mygdx.gameobjects.Bird;
+import com.mygdx.gameobjects.Grass;
+import com.mygdx.gameobjects.Pipe;
+import com.mygdx.gameobjects.ScrollHandler;
 
 /**
  * Created by njerry on 7/27/15.
  */
 public class GameWorld {
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld (int midPointY) {
         bird = new Bird(33, midPointY -5, 17, 12);
+
+        scroller = new ScrollHandler(midPointY - 66);
     }
 
     public void update (float delta) {
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird() {
         return bird;
     }
+
+    public ScrollHandler getScroller() {
+        return scroller;
+    }
 }
+
+
