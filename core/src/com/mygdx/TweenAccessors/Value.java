78f2dc6abeb9ce70bf0e16f08d0ce3195ef4e064
Sylvia <sylviakieha@gmail.com>
2015-10-09 11:36:22 +0300

    created value and value accessor classes for splash screen transition

---
+++ core/src/com/mygdx/TweenAccessors/Value.java
@@ -0,0 +1,16 @@
+package com.mygdx.TweenAccessors;
+
+/**
+ * Created by njerry on 10/9/15.
+ */
+public class Value {
+    private float val = 1;
+
+    public float getValue() {
+        return val;
+    }
+
+    public void setValue(float newVal) {
+        val = newVal;
+    }
+}
