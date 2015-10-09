8a1f132d201c9e0b23f4276142e4039682e0d04f
Sylvia <sylviakieha@gmail.com>
2015-10-09 11:50:00 +0300

    implemented methods for button rendering

--- core/src/com/mygdx/zbHelpers/InputHandler.java
+++ core/src/com/mygdx/zbHelpers/InputHandler.java
@@ -1,8 +1,13 @@
 package com.mygdx.zbHelpers;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.mygdx.gameobjects.Bird;
 import com.mygdx.gameworld.GameWorld;
+import com.mygdx.ui.SimpleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by njerry on 7/27/15.
@@ -11,13 +16,45 @@ public class InputHandler implements InputProcessor {
     private Bird myBird;
     private GameWorld myWorld;
 
-    public InputHandler(GameWorld myWorld) {
+    private List<SimpleButton> menuButtons;
+    private SimpleButton playButton;
+
+    private float scaleFactorX;
+    private float scaleFactorY;
+
+    public InputHandler(GameWorld myWorld, float scaleFactorX, float scaleFactorY) {
         this.myWorld = myWorld;
         myBird = myWorld.getBird();
+
+        int midPointY = myWorld.getMidPointY();
+
+        this.scaleFactorX = scaleFactorX;
+        this.scaleFactorY = scaleFactorY;
+
+        menuButtons = new ArrayList<SimpleButton>();
+        playButton = new SimpleButton(136 / 2 - (AssetLoader.playButtonUp.getRegionWidth() / 2), midPointY + 50, 29, 16, AssetLoader.playButtonUp, AssetLoader.playButtonDown);
+        menuButtons.add(playButton);
     }
 
     @Override
     public boolean keyDown(int keycode) {
+        // Can now use Space Bar to play the game
+        if (keycode == Input.Keys.SPACE) {
+
+            if (myWorld.isMenu()) {
+                myWorld.ready();
+            } else if (myWorld.isReady()) {
+                myWorld.start();
+            }
+
+            myBird.onClick();
+
+            if (myWorld.isGameOver() || myWorld.isHighScore()) {
+                myWorld.restart();
+            }
+
+        }
+
         return false;
     }
 
@@ -33,8 +70,13 @@ public class InputHandler implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
-        if (myWorld.isReady()) {
+        screenX = scaleX(screenX);
+        screenY = scaleY(screenY);
+        System.out.println(screenX + " " + screenY);
+        if (myWorld.isMenu()) {
+            playButton.isTouchDown(screenX, screenY);
+        }
+        else if (myWorld.isReady()) {
             myWorld.start();
         }
 
@@ -50,6 +92,16 @@ public class InputHandler implements InputProcessor {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        screenX = scaleX(screenX);
+        screenY = scaleY(screenY);
+
+        if (myWorld.isMenu()) {
+            if (playButton.isTouchUp(screenX, screenY)) {
+                myWorld.ready();
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -66,5 +118,17 @@ public class InputHandler implements InputProcessor {
     @Override
     public boolean scrolled(int amount) {
         return false;
+    }
+
+    private int scaleX(int screenX) {
+        return (int) (screenX / scaleFactorX);
+    }
+
+    private int scaleY(int screenY) {
+        return (int) (screenY / scaleFactorY);
+    }
+
+    public List<SimpleButton> getMenuButtons() {
+        return menuButtons;
     }
 }
