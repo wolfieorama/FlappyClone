ea14cf22bc521e09e393509daf3d9a94c34e037e
Sylvia <sylviakieha@gmail.com>
2015-10-12 16:23:53 +0300

    edited package name for publication

---
+++ android/src/com/njerry/game/android/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package com.njerry.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new com.njerry.game.MyGdxGame(), config);
+	}
+}
---
+++ core/src/com/njerry/zbHelpers/AssetLoader.java
@@ -0,0 +1,118 @@
+package com.njerry.zbHelpers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by njerry on 7/27/15.
+ */
+public class AssetLoader {
+    public static Texture texture, logoTexture;
+    public static TextureRegion bg, grass;
+    public static TextureRegion logo;
+
+    public static Animation birdAnimation;
+    public static TextureRegion bird, birdDown, birdUp;
+
+    public static TextureRegion skullUp, skullDown, bar;
+
+    public static Sound dead;
+    public static Sound flap;
+    public static Sound coin;
+
+    public static BitmapFont font;
+    public static BitmapFont shadow;
+
+    public static Preferences prefs;
+
+
+    public static void load() {
+
+        logoTexture = new Texture(Gdx.files.internal("data/logo.png"));
+        logoTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+        logo = new TextureRegion(logoTexture, 0, 0, 512, 114);
+
+        texture = new Texture(Gdx.files.internal("data/texture2.png"));
+        texture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+//
+//        playButtonUp = new TextureRegion(texture, 0, 83, 29, 16);
+//        playButtonDown = new TextureRegion(texture, 29, 83, 29, 16);
+//        playButtonUp.flip(false, true);
+//        playButtonDown.flip(false, true);
+
+//        zbLogo = new TextureRegion(texture, 0, 55, 135, 24);
+//        zbLogo.flip(false, true);
+
+        bg = new TextureRegion(texture, 0, 0, 136, 43);
+        bg.flip(false, true);
+
+        grass = new TextureRegion(texture, 0, 43, 143, 11);
+        grass.flip(false, true);
+
+        birdDown = new TextureRegion(texture, 136, 0, 17, 12);
+        birdDown.flip(false, true);
+
+        bird = new TextureRegion(texture, 153, 0, 17, 12);
+        bird.flip(false, true);
+
+        birdUp = new TextureRegion(texture, 170, 0, 17, 12);
+        birdUp.flip(false, true);
+
+        TextureRegion[] birds = { birdDown, bird, birdUp };
+        birdAnimation = new Animation(0.06f, birds);
+        birdAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+
+        skullUp = new TextureRegion(texture, 192, 0, 24, 14);
+        // Create by flipping existing skullUp
+        skullDown = new TextureRegion(skullUp);
+        skullDown.flip(false, true);
+
+        bar = new TextureRegion(texture, 136, 16, 22, 3);
+        bar.flip(false, true);
+
+        dead = Gdx.audio.newSound(Gdx.files.internal("data/dead.wav"));
+        flap = Gdx.audio.newSound(Gdx.files.internal("data/flap.wav"));
+        coin = Gdx.audio.newSound(Gdx.files.internal("data/coin.wav"));
+
+        font = new BitmapFont(Gdx.files.internal("data/text.fnt"));
+        font.getData().setScale(.25f, -.25f);
+        shadow =  new BitmapFont(Gdx.files.internal("data/shadow.fnt"));
+        shadow.getData().setScale(.25f, -.25f);
+
+        //create or retrieve existing preferences file
+        prefs = Gdx.app.getPreferences("ZombieBird");
+
+        //provide default high score of 0
+        if (!prefs.contains("highScore")) {
+            prefs.putInteger("highScore", 0);
+        }
+    }
+
+    public static void setHighScore(int val) {
+        prefs.putInteger("highScore", val);
+        prefs.flush();
+    }
+
+    public static int getHighScore() {
+        return prefs.getInteger("highScore");
+    }
+
+    public static void dispose() {
+        // We must dispose of the texture when we are finished.
+        texture.dispose();
+        //dispose of the Sound objects when done with them
+        dead.dispose();
+        flap.dispose();
+        coin.dispose();
+
+        font.dispose();
+        shadow.dispose();
+    }
+}
